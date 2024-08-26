ef89f59bef43900e6c4f7e422d6275ebe670113a
aamirkhan1600 <aamir.khan1600@gmail.com>
2024-08-26 18:15:39 +0530

    first commit

--- src/Login.js
+++ src/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:3000/api/login', {
+    const response = await fetch('https://exam-backend-alpha.vercel.app/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- src/Signup.js
+++ src/Signup.js
@@ -12,7 +12,7 @@
         const handleSubmit = async (e) => {
           e.preventDefault();
       
-          const response = await fetch('http://localhost:3000/api/register', {
+          const response = await fetch('https://exam-backend-alpha.vercel.app/api/register', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
