90db9fe1be818295cac83eb29f4876e1abcc484b
aamirkhan1600 <aamir.khan1600@gmail.com>
2024-08-26 18:40:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
 import Login from './Login';
 import Signup from './Signup';
 import Home from './Home';
 import AdminLogin from './AdminLogin';
 import AdminSignup from './AdminSignup';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect to /login */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home /> }/>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/AdminLogin" element={<AdminLogin />} />
-          <Route path="/AdminSignup" element={<AdminSignup />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect to /login */}
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                        <ProtectedRoute path="/home" element={Home} />
+                        <Route path="/adminlogin" element={<AdminLogin />} />
+                        <Route path="/adminsignup" element={<AdminSignup />} />
+                        {/* Optionally add protected routes for admin pages */}
+                        <ProtectedRoute path="/adminhome" element={AdminHome} />
+                    </Routes>
+                </div>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
